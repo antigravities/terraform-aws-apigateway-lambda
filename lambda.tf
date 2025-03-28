773c9afb04ef6acff8aa0922ca19142f8f80bded
Alexandra Frock <im@alexandra.moe>
2025-03-28 01:05:15 -0400

    oops, do null check instead

--- lambda.tf
+++ lambda.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "lambda" {
   timeout          = var.timeout
   architectures    = var.architectures
   memory_size      = var.memory_size
-  layers           = var.layer ? [var.layer] : []
+  layers           = var.layer != null ? [var.layer] : []
 
   environment {
     variables = var.environment
